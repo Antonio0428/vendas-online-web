5648c29e2dda40e1ba82117c897daaed3d76f230
Antonio0428 <antoniomanueldossantos.dev@gmail.com>
2025-06-07 04:19:52 +0100

    feat: insert antd and styled-components

--- vendas-online-web/src/App.tsx
+++ vendas-online-web/src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import './App.css';
 import { BemVindo } from './components/BemVindo';
+import styled from 'styled-components';
+import { Button} from 'antd';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,15 +10,22 @@ function App() {
   return (
     <>
       <BemVindo />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+      <StyledLink tornBlue={count >= 4} className="card">
+        <Button type="primary" onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-      </div>
+      </StyledLink>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
 }
+interface MudarCor4{
+  tornBlue?: boolean;
+}
+const StyledLink = styled.div<MudarCor4>`
+  color:  ${props => props.tornBlue ? "blue" : "pink"};
+  font-weight: bold;
+`;
 
 export default App;
